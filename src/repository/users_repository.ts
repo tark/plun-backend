ae7db014e7a9e8db0317b921cf977706344aa6bd
airon@tark.pro <airon@tark.pro>
2020-08-14 22:58:32 +0700

    users_repository.ts: move to TS

--- src/repository/users_repository.ts
+++ src/repository/users_repository.ts
@@ -1,30 +1,34 @@
-import UsersDb from "./db/users_db.js";
-import Logger from "../util/logger.js";
+import UsersDb from "./db/users_db";
+import Logger from "../util/logger";
+import {firestore} from "firebase";
+import {User} from "./models/models";
 
 const L = new Logger('UsersRepository');
 
 export default class UsersRepository {
 
-  constructor(db) {
+  usersDb: UsersDb;
+
+  constructor(db: firestore.Firestore) {
     this.usersDb = new UsersDb(db)
   }
 
-  get = (id) => {
+  get = (id: string) : Promise<User>=> {
     L.i(`get - ${id}`)
     return this.usersDb.getById(id);
   }
 
-  getByAzureProfileId = (azureProfileId) => {
+  getByAzureProfileId = (azureProfileId: string) : Promise<User>=> {
     L.i(`getByAzureProfileId - ${azureProfileId}`)
     return this.usersDb.getByAzureProfileId(azureProfileId);
   }
 
-  add = async (user) => {
+  add = async (user : User) : Promise<User>=> {
     L.i(`add - ${user}`)
     return this.usersDb.add(user);
   }
 
-  delete = async (id) => {
+  delete = async (id : string) => {
     L.i(`delete - ${id}`)
     return this.usersDb.delete(id);
   }

47045944259b82f801150a2276afeae1b7174ac3
Airon <airon@tark.pro>
2020-09-27 20:32:07 +0700

    tasks_controller.ts: implement working with tasks

--- src/controllers/tasks_controller.ts
+++ src/controllers/tasks_controller.ts
@@ -7,6 +7,9 @@ import {Task} from "../repository/models/models";
 
 const L = new Logger('TasksController');
 
+/**
+ * Class for business logic related to tasks
+ */
 export default class TasksController {
 
   cache: Cache;
@@ -14,7 +17,7 @@ export default class TasksController {
   tasksRepository: TasksRepository
 
   constructor(db: firestore.Firestore, cache: Cache, azureApi: AzureApi) {
-    this.tasksRepository = new TasksRepository(db);
+    this.tasksRepository = new TasksRepository(db, azureApi);
     this.azureApi = azureApi
     this.cache = cache
   }
@@ -24,12 +27,67 @@ export default class TasksController {
                           query: string,
                           token: string) => {
     L.i(`getSuggestions`)
-    return this.azureApi.getTasksNew(organizationName, projectName, query, token);
+    return this.azureApi.searchTasks(organizationName, projectName, query, token);
   }
 
   planTasks = async (tasks: Array<Task>) => {
     L.i(`planTasks`)
-    await this.tasksRepository.addTasks(tasks.map(t => ({...t, plannedAt: new Date().getTime()})));
+    const todayMidnight = new Date();
+    todayMidnight.setHours(0);
+    todayMidnight.setMinutes(0);
+    todayMidnight.setSeconds(0);
+    todayMidnight.setMilliseconds(0);
+    return await this.tasksRepository.addTasks(tasks.map(t => ({
+      ...t,
+      plannedAt: todayMidnight.getTime()
+    })));
+  }
+
+  /**
+   * Returns planned tasks by given date
+   */
+  getPlannedTasks = (organizationName: string,
+                     projectName: string,
+                     date: number,
+                     token: string) : Promise<Array<Task>> => {
+    L.i(`getPlannedTasks - ${new Date(date)}`)
+    const dateMidnight = new Date(date)
+    dateMidnight.setHours(0);
+    dateMidnight.setMinutes(0);
+    dateMidnight.setSeconds(0);
+    dateMidnight.setMilliseconds(0);
+    return this.tasksRepository.getTasksByDate(
+      organizationName,
+      projectName,
+      dateMidnight.getTime(),
+      token
+    );
+  }
+
+  /**
+   * Returns nearest available planned tasks
+   * Looks for what nearest day has tasks
+   * After found it - return all the tasks for this day
+   */
+  getPreviousNearestTasks = (organizationName: string,
+                            projectName: string,
+                            token: string) => {
+
+    return this.tasksRepository.getPreviousNearestTasks(
+      organizationName,
+      projectName,
+      token
+    );
+
+
+  }
+
+  deleteTask = (taskId: string) => {
+    return this.tasksRepository.delete(taskId);
+  }
+
+  updateTask = (task: Task) : Promise<Task> => {
+    return this.tasksRepository.update(task);
   }
 
 }

16a3b3dc6cb5463a676c6aa5f64ca64abf314aae
Airon <airon@tark.pro>
2020-09-27 20:32:28 +0700

    tasks_db.ts: implement working with tasks

--- src/repository/db/tasks_db.ts
+++ src/repository/db/tasks_db.ts
@@ -1,5 +1,9 @@
 import {firestore} from "firebase";
 import {Task} from "../models/models";
+import moment from "moment";
+import Logger from "../../util/logger";
+
+const L = new Logger('TasksDb');
 
 export default class TasksDb {
 
@@ -19,6 +23,40 @@ export default class TasksDb {
     return this.mapDocToTask(doc);
   }
 
+  getByDate = async (date: number): Promise<Array<Task>> => {
+    const querySnapshot = await this.tasks().where("plannedAt", "==", date).get();
+    const tasks = new Array<Task>()
+    querySnapshot.forEach((doc) => {
+      tasks.push(this.mapDocToTask(doc))
+    });
+    return tasks;
+  }
+
+  /**
+   * Returns list of tasks for the nearest day before that have at least one task
+   */
+  getPreviousNearestTasks = async (): Promise<Array<Task>> => {
+    L.i(`getForNearestDate`)
+    const now = moment().valueOf();
+
+    // get the nearest task
+    const querySnapshot = await this.tasks()
+      .where("plannedAt", "<", now)
+      .orderBy("plannedAt", 'desc')
+      .limit(1)
+      .get();
+
+    if (querySnapshot.empty){
+      return [];
+    }
+
+    // get the date
+    const date = querySnapshot.docs[0].data()['plannedAt'];
+
+    //get all tasks by this date
+    return this.getByDate(date);
+  }
+
   /*getByName = async (name: string) => {
     return this.tasks().where("name", ">", name)
   }*/
@@ -31,14 +69,22 @@ export default class TasksDb {
 
   addTasks = async (tasks: Array<Task>): Promise<boolean> => {
     await Promise.all(tasks.map(async t => {
-      await this.tasks().add(t)
+      const tasksWithSameAzureId = await this.tasks().where("azureId", "==", t.azureId ?? '').get()
+      if (tasksWithSameAzureId.empty) {
+        await this.tasks().add(t)
+      }
     }))
     return true;
   }
 
-  delete = async (id: string) => {
-    const task = await this.getById(id);
-    //await task.delete();
+  delete = (id: string) => {
+    return this.tasks().doc(id).delete()
+  }
+
+  update = async (task: Task) : Promise<Task> => {
+    const ref = this.tasks().doc(task.id);
+    await ref.update(task);
+    return this.getById(task.id);
   }
 
   tasks = (): firestore.CollectionReference => {
@@ -46,14 +92,13 @@ export default class TasksDb {
   }
 
   mapDocToTask = (doc: firestore.DocumentSnapshot): Task => {
-    const {azureId, azureName, azureUrl, azureState, name, state, plannedAt} = doc.data()
+    const {azureId, azureUrl, azureState, name, state, plannedAt} = doc.data()
     return {
       id: doc.id,
       name,
       state,
       plannedAt,
       azureId,
-      azureName,
       azureUrl,
       azureState,
     };

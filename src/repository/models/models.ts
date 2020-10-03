1937c7f2345862331e8da237ab4de97afbf28a50
Airon <airon@tark.pro>
2020-10-03 12:24:43 +0700

    models.ts: switch plannedAt and state to arrays.
    Because the task can lasts a few days - we should keep planned days and states for all these days.

--- src/repository/models/models.ts
+++ src/repository/models/models.ts
@@ -22,12 +22,23 @@ export type Project = {
 export type Task = {
   id: string;
   name: string;
-  state: TaskState;
-  plannedAt: number;
+  // array of states
+  // for example for the first day task was "created"
+  // for the second day state was "in progress"
+  // and for the third day it becomes "done"
+  state: Array<DayState>;
+  // all the days when task was planned
+  // for example one task can moves from one day to another
+  plannedAt: Array<number>;
   azureId?: number;
   azureState?: TaskState;
   azureUrl?: string;
 };
+
+export type DayState = {
+  date: number,
+  state: TaskState,
+}
 
 export type TaskState = 'created' | 'done' | 'progress' | 'failed' | 'cancelled'
 

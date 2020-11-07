3de8706438aaf71c1efd5fcb72c533600ad2a8a3
Airon <airon@tark.pro>
2020-11-07 12:50:30 +0700

    users_controller.ts: implement new way of using token

--- src/controllers/users_controller.ts
+++ src/controllers/users_controller.ts
@@ -5,7 +5,7 @@ import OrganizationsRepository from "../repository/organizations_repository";
 import ProjectsRepository from "../repository/projects_repository";
 import AzureApi from "../repository/api/azure_api";
 import Cache from "../repository/cache";
-import {Organization, Project, User} from "../repository/models/models";
+import {AzureAuthResponse, Organization, Project, User} from "../repository/models/models";
 import {firestore} from "firebase";
 
 const L = new Logger('UsersController');
@@ -27,61 +27,52 @@ export default class UsersController {
     this.azureApi = azureApi;
   }
 
-  /**
-   *
-   * Not using this. We are not auth by user id anymore,
-   * use token for this.
-   *
-   * @deprecated
-   * @param userId
-   */
-  auth = async (userId: string) => {
-    L.i(`auth - ${userId}`);
-    const token = this.getTokenFromCache(userId);
-
-    // check if token not expired by trying to call an api
-    // todo in the future check the access token expired time
-    // we can save it once received and then check
-    try {
-      return await this.initUser(token);
-    } catch (e) {
-      throw new UnauthorizedError()
-    }
+  refreshToken = async (refreshToken: string): Promise<AzureAuthResponse> => {
+    return this.azureApi.refreshToken(refreshToken);
   }
 
   /**
+   * Auth by given code and create a user
+   * if this user was not created before
+   *
    * This function used when we have no access token or the token is expired
    * We are using the given authorization code to get the access token.
    * More details here
    * https://docs.microsoft.com/en-us/azure/devops/integrate/get-started/authentication/oauth
+   *
+   * @return Promise<AzureAuthResponse>
    */
-  authByCode = async (authCode: string) => {
-    L.i(`authByCode`)
+  authByCode = async (authCode: string) : Promise<AzureAuthResponse> => {
+    L.i(`authByCode - ${authCode}`)
 
-    const token = await this.azureApi.getAccessToken(authCode);
+    const authResponse = await this.azureApi.auth(authCode);
 
-    const savedUser = await this.initUser(token);
+    const azureProfile = await this.azureApi.getProfile(authResponse.accessToken)
 
-    this.cache.setToken(savedUser.id, token)
+    const {emailAddress, displayName, id} = azureProfile
 
-    return savedUser;
-
-  }
-
-  getAccessToken = async (authCode: string) => {
-    return this.azureApi.getAccessToken(authCode);
-  }
-
-  getProfile = async (token: string) : Promise<User> => {
-    this.checkToken(token)
-    const azureProfile = await this.azureApi.getProfile(token)
-    const {displayName, emailAddress, id} = azureProfile
-    return {
-      id: '',
-      azureProfileId: id,
-      name: displayName,
-      email: emailAddress,
+    let savedUser: User = await this.usersRepository.getByAzureProfileId(id);
+    if (!savedUser) {
+      // save user if we don't have one
+      await this.usersRepository.add({
+        id: savedUser.id,
+        azureProfileId: id,
+        name: displayName,
+        email: emailAddress,
+      })
     }
+
+    return authResponse;
+
+  }
+
+  /*getAccessToken = async (authCode: string) => {
+    return this.azureApi.getAccessToken(authCode);
+  }*/
+
+  getProfile = async (token: string): Promise<User> => {
+    const azureProfile = await this.azureApi.getProfile(token)
+    return this.usersRepository.addOrUpdateByAzureId(azureProfile);
   }
 
   /**
@@ -90,6 +81,12 @@ export default class UsersController {
    * I am not really sure if we need to save the user, orgs and projects to DB
    * maybe we can to save only tasks?
    * @param token
+   *
+   * Let's not do so much work at start.
+   * Let's only return a user and let other components ask for organizations ' projects themselv
+   *
+   * @deprecated
+   *
    */
   initUser = async (token: string) => {
     L.i(`initUser`)
@@ -122,37 +119,20 @@ export default class UsersController {
     return savedUser;
   }
 
-  getOrganizations = async (token: string) : Promise<Array<Organization>> => {
+  getOrganizations = async (token: string): Promise<Array<Organization>> => {
     L.i(`getOrganizations`)
     this.checkToken(token)
     return this.azureApi.getOrganizations(token);
   }
 
-  getProjects = async (organizationName: string, token: string) : Promise<Array<Project>> => {
+  getProjects = async (organizationName: string, token: string): Promise<Array<Project>> => {
     L.i(`getProjects - ${organizationName}`)
     this.checkToken(token)
     return this.azureApi.getProjects(organizationName, token);
   }
 
-  /**
-   * Will keep token on a client side, no need to keep it here
-   * @deprecated
-   * @param userId
-   */
-  getTokenFromCache = (userId: string): string => {
-    const token = this.cache.getToken(userId);
-
-    // check if token is here
-    if (!token) {
-      L.i(`auth - error`)
-      throw new UnauthorizedError()
-    }
-
-    return token
-  }
-
   checkToken = (token: string) => {
-    if (!token){
+    if (!token) {
       throw new UnauthorizedError();
     }
   }

b96987eae8793b61c1ebf2e3f30ff75a8471344c
Airon <airon@tark.pro>
2020-11-07 12:51:06 +0700

    users_repository.ts: code refactor

--- src/repository/users_repository.ts
+++ src/repository/users_repository.ts
@@ -1,7 +1,7 @@
 import UsersDb from "./db/users_db";
 import Logger from "../util/logger";
 import {firestore} from "firebase";
-import {User} from "./models/models";
+import {AzureProfile, User} from "./models/models";
 
 const L = new Logger('UsersRepository');
 
@@ -13,24 +13,44 @@ export default class UsersRepository {
     this.usersDb = new UsersDb(db)
   }
 
-  get = (id: string) : Promise<User>=> {
+  get = (id: string): Promise<User> => {
     L.i(`get - ${id}`)
     return this.usersDb.getById(id);
   }
 
-  getByAzureProfileId = (azureProfileId: string) : Promise<User>=> {
+  getByAzureProfileId = (azureProfileId: string): Promise<User> => {
     L.i(`getByAzureProfileId - ${azureProfileId}`)
     return this.usersDb.getByAzureProfileId(azureProfileId);
   }
 
-  add = async (user : User) : Promise<User>=> {
+  add = async (user: User): Promise<User> => {
     L.i(`add - ${user}`)
     return this.usersDb.add(user);
   }
 
-  delete = async (id : string) => {
+  addOrUpdateByAzureId = async (azureProfile: AzureProfile): Promise<User> => {
+    L.i(`addOrUpdateByAzureId - ${azureProfile.emailAddress}`)
+    const userFromDb = await this.getByAzureProfileId(azureProfile.id)
+    if (!userFromDb) {
+      return this.usersDb.add(this.userFromAzureProfile(azureProfile));
+    } else {
+      return this.usersDb.update(userFromDb);
+    }
+  }
+
+  delete = async (id: string) => {
     L.i(`delete - ${id}`)
     return this.usersDb.delete(id);
+  }
+
+  userFromAzureProfile = (azureProfile: AzureProfile) : User => {
+    const {emailAddress, displayName, id} = azureProfile
+    return {
+      id: '',
+      azureProfileId: id,
+      name: displayName,
+      email: emailAddress,
+    };
   }
 
 }
